cc7ceb85cc72b73e5d494094c76e65f1fa6072e3
Vadym Pasko <keenon3d@gmail.com>
2016-04-27 00:15:37 +0300

    Start analysis of asteroid orbital parameters distributions.

--- generate_orbits.py
+++ generate_orbits.py
@@ -2,9 +2,70 @@ import numpy as np
 import pickle 
 import pandas as pd
 # from read_database import calc_moid, get_hazMOID
-
+import scipy.stats as ss
 import read_database as rdb
+import matplotlib.pyplot as plt
 # from learn_data import loadObject, dumpObject
+
+
+def get_density(data, num=20):
+    dmin, dmax = min(data), max(data)
+    bounds = np.linspace(dmin, dmax, num)
+    bounds_centers = np.array([(a+b)*0.5 for a, b in zip(bounds[:-1], bounds[1:])])
+    density = np.histogram(data, bins=bounds, density=True)[0]
+    print "density:", density, len(density)
+    print "bounds:", bounds, len(bounds)
+    # h = plt.hist(density, bins=range(num), color='y')
+    gamma_pdf = ss.gamma
+    a, loc, scale = gamma_pdf.fit(density)
+    print "a:", a
+    # pdf_fitted = gamma_pdf.pdf(bounds, *param[:-2], loc=param[-2], scale=param[-1])
+    # bb = np.linspace(dmin, dmax, num*3)
+    pdf_fitted = gamma_pdf.pdf(bounds, a)
+    print "pdf_fitted:", pdf_fitted
+    # n, bins, patches = plt.hist(density, bins=range(len(density))) # bins=bounds
+    # gaussian_numbers = np.random.randn(1000)
+    # print "gaussian_numbers:", gaussian_numbers
+    # plt.hist(gaussian_numbers, bins=bounds)
+    plt.bar(bounds_centers, density, 0.12)
+    l = plt.plot(bounds, pdf_fitted, 'r--', linewidth=1)
+    plt.show()
+
+
+
+
+def gen_rand_asteroid(data_arr):
+    params = get_param_bounds(data_arr)
+    # print "params a:", params['a'][0], params['a'][1]
+    a = np.random.uniform(low=params['a'][0], high=params['a'][1], size=1)[0]
+    i = np.random.uniform(low=params['i'][0], high=params['i'][1], size=1)[0]
+    w = np.random.uniform(low=params['w'][0], high=params['w'][1], size=1)[0]
+    om = np.random.uniform(low=params['om'][0], high=params['om'][1], size=1)[0]
+    q = np.random.uniform(low=params['q'][0], high=params['q'][1], size=1)[0]
+    e = (a - q)/a
+    return a, e, w, i, om
+
+
+
+
+
+def get_param_bounds(data_arr):
+    amin, amax = np.min(data_arr[:,0]), np.max(data_arr[:,0])
+    # emin, emax = np.min(data_arr[:,1]), np.max(data_arr[:,1])
+    imin, imax = np.min(data_arr[:,2]), np.max(data_arr[:,2])
+    wmin, wmax = np.min(data_arr[:,3]), np.max(data_arr[:,3])
+    omin, omax = 0.0, 360.0
+    qmin, qmax = np.min(data_arr[:,5]), np.max(data_arr[:,5])
+    params = {'a': (amin, amax), 
+              'i': (imin, imax), 
+              'w': (wmin, wmax), 
+              'om': (omin, omax), 
+              'q': (qmin, qmax)}
+    return params
+
+
+
+
 
 def gen_rand(data_arr, num=100):
     amin, amax = np.min(data_arr[:,0]), np.max(data_arr[:,0])
@@ -41,21 +102,27 @@ if __name__ == '__main__':
 
     data_full = np.concatenate((hazdata_arr, nohazdata_arr))
 
-    print "init orbit generation..."
-    randdata = gen_rand(data_full, num=1e2)
-    print "orbit generation finished."
 
-    dataframe = pd.DataFrame(randdata, columns=['a', 'e', 'i', 'w', 'om', 'q'])
-    ### CALCULATE MOID ###
-    print "init MOID copmutation..."
-    data = rdb.calc_moid(dataframe)
-    print "MOID copmutation finished."
 
-    # print "randdata sample:", randdata[:10]
-    print "dataframe:\n", dataframe[:10]
 
-    haz, nohaz = rdb.get_hazMOID(data)
 
-    rdb.dumpObject(haz, './asteroid_data/haz_rand_test.p')
-    rdb.dumpObject(nohaz, './asteroid_data/nohaz_rand_test.p')
+    get_density(hazdata_arr[:,0])
+
+    # print "init orbit generation..."
+    # randdata = gen_rand(data_full, num=1e2)
+    # print "orbit generation finished."
+
+    # dataframe = pd.DataFrame(randdata, columns=['a', 'e', 'i', 'w', 'om', 'q'])
+    # ### CALCULATE MOID ###
+    # print "init MOID copmutation..."
+    # data = rdb.calc_moid(dataframe)
+    # print "MOID copmutation finished."
+
+    # # print "randdata sample:", randdata[:10]
+    # print "dataframe:\n", dataframe[:10]
+
+    # haz, nohaz = rdb.get_hazMOID(data)
+
+    # rdb.dumpObject(haz, './asteroid_data/haz_rand_test.p')
+    # rdb.dumpObject(nohaz, './asteroid_data/nohaz_rand_test.p')
 
