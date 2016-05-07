678ef01b0078ae14ed89a35526580c7f49e0b346
Vadym Pasko <keenon3d@gmail.com>
2016-05-08 02:05:18 +0300

    Add function for downloading asteroid database. Add ipython notebook.

--- read_database.py
+++ read_database.py
@@ -3,12 +3,27 @@ from multiprocessing import Pool, Lock
 import numpy as np
 import pandas as pd
 from numpy.linalg import norm
-import pickle  
+import pickle, urllib
 from functools import partial
 import calculate_orbits as co
 
 
+print_msg = {
+    'calc_moid_start': "init MOID copmutation...",
+    'calc_moid_finish': "MOID copmutation finished in %f seconds.",
+    'download_start': "Downloading database...",
+    'download_finish': "Download finished in %f seconds."
+    }
 
+def retrieve_database(database_url, filepath, verbose=False):
+    if verbose:
+        print print_msg['download_start']
+    t0 = time.time()
+    database = urllib.URLopener()
+    database.retrieve(database_url, filepath)
+    t1 = time.time()
+    if verbose:
+        print print_msg['download_finish'] % (t1-t0)
 
 def loadObject(fname):
     obj_file = open(fname,'r')
@@ -79,8 +94,11 @@ def append_moid(ir):
     # data.set_value(index, 'moid', moid)
     return (index, moid)
 
-def calc_moid(data):
+def calc_moid(data, verbose=False):
     """append column with values of moid"""
+    if verbose:
+        print print_msg['calc_moid_start']
+    t0 = time.time()
     corenums = multiprocessing.cpu_count()
     if corenums > 1:
         corenums -= 1
@@ -92,12 +110,21 @@ def calc_moid(data):
     pool.join()
     for index, moid in mapresult.get():
         data.set_value(index, 'moid', moid)
+    t1 = time.time()
+    if verbose:
+        print print_msg['calc_moid_finish'] % (t1-t0)
 
-def calc_moid_1thread(data):
+def calc_moid_1thread(data, verbose=False):
+    if verbose:
+        print print_msg['calc_moid_start']
+    t0 = time.time()
     for index, row in data.iterrows():
         w_, i_, om_ = np.radians([row.w, row.i, row.om])
         moid = co.get_moid(row.a, row.e, w_, i_, om_)
         data.set_value(index, 'moid', moid)
+    t1 = time.time()
+    if verbose:
+        print print_msg['calc_moid_finish'] % (t1-t0)
 
 def calc_rascend(data):
     """append column with values of ascending node distance"""
@@ -105,7 +132,6 @@ def calc_rascend(data):
         a, e, w_ = row.a, row.e, np.radians(row.w)
         r = co.get_r(a, e, w_)
         data.set_value(index, 'rasc', r)
-    # return data
 
 def calc_rclose(data):
     """append columns with values of clocect distance
@@ -116,7 +142,6 @@ def calc_rclose(data):
         rx, ry = co.get_rxry(a, e, w_, i_, om_)
         data.set_value(index, 'rx', rx)
         data.set_value(index, 'ry', rx)
-    # return data
 
 def calc_orbc(data):
     """append columns with values of orbit center coordinates"""
@@ -127,22 +152,29 @@ def calc_orbc(data):
         data.set_value(index, 'cx', c[0])
         data.set_value(index, 'cy', c[1])
         data.set_value(index, 'cz', c[2])
-    # return data
 
 
 
 if __name__ == '__main__':
 
     ### READ ASTEROID DATABASE ###
-    folder_path = sys.path[0]
-    database_path = os.path.join(folder_path, os.path.dirname(""),
-                    "./asteroid_data/latest_fulldb.csv")
-    # database = pd.read_csv(database_path, sep=',', 
-    #            usecols=[0,4,6,7,8,9,15,18,19,32,33,34,35,36,37,43,44,45,46,47,48,49])
-    database = pd.read_csv(database_path, sep=',', usecols=['a', 'e', 'i', 'w', 'om', 'q',
-                                                            'H', 'neo', 'pha', 'moid', 'per',
-                                                            'n', 'ma', 'epoch'],
-                                                            low_memory=False)
+    # reload(sys)
+    # sys.path += ['.']
+    database_path = "./asteroid_data/latest_fulldb.csv"
+    if not os.path.exists(database_path):
+        database_url = "http://www.ianww.com/latest_fulldb.csv"
+        retrieve_database(database_url, database_path, verbose=True)
+
+
+    # folder_path = sys.path[0]
+    # database_path = os.path.join(folder_path, os.path.dirname(""),
+    #                 "./asteroid_data/latest_fulldb.csv")
+    # database = pd.read_csv(database_path, sep=',', usecols=[0,4,6,7,8,9,15,
+    #                        18,19,32,33,34,35,36,37,43,44,45,46,47,48,49])
+    database = pd.read_csv(database_path, sep=',', usecols=[
+                           'a', 'e', 'i', 'w', 'om', 'q', 'H', 'neo',
+                           'pha', 'moid', 'per', 'n', 'ma', 'epoch'], 
+                           low_memory=False)
 
     db_head = database[:10]
     print "db_head:\n", db_head
@@ -151,11 +183,7 @@ if __name__ == '__main__':
     neo, num_neo = get_neo(database)
 
     ### RECALCULATE MOID BASED ON ORBITAL PARAMETERS ###
-    print "init MOID copmutation..."
-    t0 = time.time()
-    calc_moid(neo)
-    t1 = time.time() - t0
-    print "MOID copmutation finished in %f seconds." % t1
+    calc_moid(neo, verbose=True)
 
     ### ADD ASCENDING NODE DISTANCE ###
     # calc_rascend(neo)
